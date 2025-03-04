6a63bf197c673462f9926667b48592908316de0c
lalyos <lalyos@yahoo.com>
2025-03-04 12:04:43 +0100

    volume

---
+++ sql/init.sql
@@ -0,0 +1,6 @@
+create table vip (name varchar(20), age integer, money integer);
+insert into vip VALUES ('victor', 64, 0);
+insert into vip VALUES ('laurence', 64, 200);
+insert into vip VALUES ('feri', 62, 10);
+insert into vip VALUES ('csicsolina', 88, 1);
+insert into vip VALUES ('klara', 60, 3);
